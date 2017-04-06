d3049223db5cc9f8eff17c239a9aa16f88218242
EltonRst <eltonreisdorfer2@gmail.com>
2017-04-06 18:00:19 -0300

    Added Library Core class

---
+++ src/PHP/QPDF/QPDF.php
@@ -0,0 +1,15 @@
+<?php
+	namespace PHP\QPDF;
+	
+	class QPDF
+	{
+		/**
+		* Random name that will be the name of the temporary files
+		*
+		* @var string
+		*/
+		protected $fileName;
+		
+		
+	}
+?>
